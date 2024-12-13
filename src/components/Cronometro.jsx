c7c7ed2b8b15ff89660db124904216eccea525e7
GabrieldeSouzaGomes <166565269+GabrieldeSouzaGomes@users.noreply.github.com>
2024-12-12 23:32:34 -0300

    Add files via upload

---
+++ src/components/Cronometro.jsx
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from 'react', 
+
+function Cronometro() {
+    const [segundos,setSegundos] = useState(0); 
+    const [minutos,setMinutos] = useState(0); 
+    const [horas,setHoras] = useState(0); 
+    const [iniciado,setIniciado] = useState(false);  
+
+    useEffect(() => {
+        let
+    }
+     )
+}
